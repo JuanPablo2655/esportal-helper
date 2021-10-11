1f304ae2ee15a583ffdf0342f0fab4344b57504b
Jari <34107546+notJari@users.noreply.github.com>
2021-10-12 00:55:31 +0300

    stats command
    check the stats of the site, online, queued, live gathers, public server players

---
+++ commands/esportal/stats.ts
@@ -0,0 +1,80 @@
+import { ICommand } from "wokcommands";
+import DiscordJS, { Message, MessageEmbed } from 'discord.js';
+import axios from "axios";
+
+export default {
+    category: 'esportal',
+    description: 'Checks Recent Esportal Matches.',
+
+    maxArgs: 0,
+
+    slash: 'both',
+    testOnly: false,
+    ownerOnly: false,
+
+    callback: async ({ }) => {
+
+        const errorembed = new MessageEmbed()
+        .setTitle(`An error has occured.`)
+        .setDescription(`Please try again later.`)
+        .setColor('RED')
+        
+        try {
+            
+        var res = await axios
+            .get(`https://api.esportal.com/matchmaking/stats`) as any
+        
+        }
+            
+        catch {
+
+        return errorembed
+        
+        }
+
+        let data = res.data;
+        //console.log(data)
+
+        const embed = new MessageEmbed()
+            .setTitle(`View Esportal`)
+            .setURL(`https://esportal.com/home`)
+            .setColor('AQUA')
+            .addFields([
+                {
+                name: 'Online Users',
+                value: `${data.online_users}`,
+                inline: true
+                },
+                {
+                name: 'Queued Users',
+                value: `${data.queued_users}`,
+                inline: true
+                },
+                {
+                name: '\u200B',
+                value: '\u200B',
+                inline: true
+                },
+                {
+                name: 'Live Gathers',
+                value: `${data.live_gathers}`,
+                inline: true
+                },
+                {
+                name: 'Public Server Players',
+                value: `${data.public_server_players}`,
+                inline: true
+                },
+                {
+                name: '\u200B',
+                value: '\u200B',
+                inline: true
+                }
+            ])
+
+        return embed
+
+    },
+} as ICommand
+
+// /user

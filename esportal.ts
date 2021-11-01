192a039dfeeb2814a963b4f77971a312192a28f7
Jari <34107546+notJari@users.noreply.github.com>
2021-11-01 13:33:02 +0200

    top-gg api

--- esportal.ts
+++ esportal.ts
@@ -1,80 +1,82 @@
-import DiscordJS, { Intents, Interaction } from 'discord.js'
+import DiscordJS, { Intents } from 'discord.js'
+import { AutoPoster } from 'topgg-autoposter'
 import WOKCommands from 'wokcommands'
 import path from 'path'
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import axios from "axios";
+import axios from "axios"
 dotenv.config()
 
 const client = new DiscordJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.DIRECT_MESSAGES,
-        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
-    ]
+intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
+]
 })
 
-client.on ('ready', () => {
+const ap = AutoPoster(process.env.TOPGG || '', client)
 
-    const wok = new WOKCommands(client, {
-        commandsDir: path.join(__dirname, 'commands'),
-        typeScript: true,
-        testServers: ['896403600102801410'],
-        botOwners: ['817275612430336022'],
+ap.on('posted', () => {
+  console.log('Posted stats to Top.gg!')
+})
 
-        disabledDefaultCommands: [
-            'help',
-            'command',
-            'language',
-            'prefix',
-            'requiredrole',
-            'channelonly'
-        ],
-
-        mongoUri: process.env.DB,
-        // dbOptions: {
-        //     keepAlive: true
-        // }
-        debug: true,
+client.on('ready', () => {
+const wok = new WOKCommands(client, {
+    commandsDir: path.join(__dirname, 'commands'),
+    typeScript: true,
+    testServers: ['896403600102801410'],
+    botOwners: ['817275612430336022'],
+    disabledDefaultCommands: [
+        'help',
+        'command',
+        'language',
+        'prefix',
+        'requiredrole',
+        'channelonly'
+    ],
+    mongoUri: process.env.DB,
+    // dbOptions: {
+    //     keepAlive: true
+    // }
+    debug: true,
+    
+})
+.setDefaultPrefix('e!')
+})
+let statuses = 0;
+setInterval ( async () => {
+    try {     
+        var res = await axios
+            .get(`https://api.esportal.com/matchmaking/stats`) as any    
+        }          
+        catch {
         
+        }
+        let esportal = res.data;
+        //console.log(data)
+        const arrayOfStatus = [
+            `Serving ${client.guilds.cache.size} servers!`,
+            `Prefix is: 'e!'`,
+            `Currently Online: ${esportal.online_users}`,
+            `Currently in Queue: ${esportal.queued_users}`,
+            `Live Gather Matches: ${esportal.live_gathers}`,
+        ];
+        
+        statuses++;
+        if(statuses >= arrayOfStatus.length) statuses = 0;
+        const status = arrayOfStatus[statuses];
+        console.log(status)
+        client.user?.setPresence({
+        activities: [
+            {
+                name: status,
+                type: 'STREAMING',
+                url: "https://twitch.tv/esportal"
+            },
+        ],
     })
-    .setDefaultPrefix('e!')
-    })
-
-    let statuses = 0;
-    setInterval ( async () => {
-        try {     
-            var res = await axios
-                .get(`https://api.esportal.com/matchmaking/stats`) as any    
-            }          
-            catch {
-            
-            }
-            let esportal = res.data;
-            //console.log(data)
-            const arrayOfStatus = [
-                `Serving ${client.guilds.cache.size} servers!`,
-                `Prefix is: 'e!'`,
-                `Currently Online: ${esportal.online_users}`,
-                `Currently in Queue: ${esportal.queued_users}`,
-                `Live Gather Matches: ${esportal.live_gathers}`,
-            ];
-            
-            statuses++;
-            if(statuses >= arrayOfStatus.length) statuses = 0;
-            const status = arrayOfStatus[statuses];
-            console.log(status)
-            client.user?.setPresence({
-            activities: [
-                {
-                    name: status,
-                    type: 'STREAMING',
-                    url: "https://twitch.tv/esportal"
-                },
-            ],
-        })
-    }, 60000)
+}, 60000)
 
 client.login(process.env.TOKEN)

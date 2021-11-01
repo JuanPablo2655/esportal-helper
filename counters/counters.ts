ac57498411834229196c8bf2fe0f97224fc9da5a
Jari <34107546+notJari@users.noreply.github.com>
2021-11-01 15:23:12 +0200

    counter fix

--- counters/counters.ts
+++ counters/counters.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-module.exports = async (client: { channels: { fetch: (arg0: string) => any; }; guilds: { cache: { size: any; }; }; }) => {
+module.exports = async (client: { channels: { fetch: (arg0: string) => any; }; guilds: { cache: { size: any; }; }; users: { cache: { size: any; }; }; }) => {
 
     try {
         var res = await axios
@@ -11,13 +11,13 @@ module.exports = async (client: { channels: { fetch: (arg0: string) => any; }; g
     let esportal = res.data;
 
     const serving = await client.channels.fetch('903626317667393606');
-    const users = await client.channels.fetch('903626444729647114');
+    const userschannel = await client.channels.fetch('903626444729647114');
     const online = await client.channels.fetch('903626609339277313');
     const queue = await client.channels.fetch('903626671683428393');
     const live = await client.channels.fetch('903626741313052772');
 
     serving.setName(`Serving: ${client.guilds.cache.size} Servers`)
-    // users.setName(`Serving ${idontrememberthetexttoputhere} Users`)
+    userschannel.setName(`Serving ${client.users.cache.size} Users`)
     online.setName(`Online on Esportal: ${esportal.online_users}`)
     queue.setName(`In Queue: ${esportal.queued_users}`)
     live.setName(`Live Gathers: ${esportal.live_gathers}`)

ccf843e6602c261faac5fcaba89fb1d905af492f
Jari <34107546+notJari@users.noreply.github.com>
2021-11-02 21:04:11 +0200

    fix

--- features/autoposter/autoposter.ts
+++ features/autoposter/autoposter.ts
@@ -1,4 +1,6 @@
 import { AutoPoster } from 'topgg-autoposter'
+import dotenv from 'dotenv'
+dotenv.config()
 
 module.exports = async (client: any) => {
   const ap = AutoPoster(process.env.TOPGG || '', client)

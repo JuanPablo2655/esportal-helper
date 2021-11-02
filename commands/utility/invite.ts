1a44aa00c9b6d98020a9bb60c030f4d69b54301e
Jari <34107546+notJari@users.noreply.github.com>
2021-11-02 20:13:54 +0200

    new commands

---
+++ commands/utility/invite.ts
@@ -0,0 +1,24 @@
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Utility',
+    description: 'Gives you the Support Server Invite.',
+
+    slash: 'both',
+    testOnly: false,
+    ownerOnly: false,
+
+    maxArgs: 0,
+
+    callback: ({ message, interaction }) => {
+        if(message) {
+            message.reply('You can invite the bot at: https://dsc.gg/esportalbot')
+        }
+        if (interaction) {
+            interaction.reply({
+                content: 'You can invite the bot at: https://dsc.gg/esportalbot',
+                ephemeral: true,
+            })
+        }
+    },
+} as ICommand
---
+++ commands/utility/support.ts
@@ -0,0 +1,24 @@
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Utility',
+    description: 'Gives you the Support Server Invite.',
+
+    slash: 'both',
+    testOnly: false,
+    ownerOnly: false,
+
+    maxArgs: 0,
+
+    callback: ({ message, interaction }) => {
+        if(message) {
+            message.reply('You can join my support server at: https://dsc.gg/esportalhelper')
+        }
+        if (interaction) {
+            interaction.reply({
+                content: 'You can join my support server at: https://dsc.gg/esportalhelper',
+                ephemeral: true,
+            })
+        }
+    },
+} as ICommand

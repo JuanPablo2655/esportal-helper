ad1ed92cc5553aa737d0c4be72734f5fc58d57d4
Jari <34107546+notJari@users.noreply.github.com>
2021-11-03 13:07:39 +0200

    status for testing bot

---
+++ features/alphastatus.ts
@@ -0,0 +1,25 @@
+module.exports = async (client: { guilds: { cache: { size: any; }; }; user: { setPresence: (arg0: { activities: { name: string; type: string; url: string; }[]; }) => void; }; }) => {
+    let statuses = 0;
+    setInterval(async () => {
+        // console.log(data)
+        const arrayOfStatus = [
+            `This is Esportal Helper Canary!`,
+            `Prefix is: 'e!'`,
+            `Testing is fun.'`,
+        ];
+        
+        statuses++;
+        if (statuses >= arrayOfStatus.length) statuses = 0;
+        const status = arrayOfStatus[statuses];
+        // console.log(status)
+        client.user?.setPresence({
+            activities: [
+                {
+                    name: status,
+                    type: 'STREAMING',
+                    url: "https://twitch.tv/esportal"
+                },
+            ],
+        })
+    }, 30000)
+}

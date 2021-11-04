190db8126d8aec9809f4ac536c515bfdbb23a1d5
Jari <34107546+notJari@users.noreply.github.com>
2021-11-04 09:29:10 +0200

    Create vote.ts

---
+++ commands/other/vote.ts
@@ -0,0 +1,24 @@
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Other',
+    description: 'Gives you the Voting.',
+
+    slash: 'both',
+    testOnly: false,
+    ownerOnly: false,
+
+    maxArgs: 0,
+
+    callback: ({ message, interaction }) => {
+        if(message) {
+            message.reply('You can Vote the bot at: https://top.gg/bot/896400457545695232')
+        }
+        if (interaction) {
+            interaction.reply({
+                content: 'You can Vote the bot at: https://top.gg/bot/896400457545695232',
+                ephemeral: true,
+            })
+        }
+    },
+} as ICommand

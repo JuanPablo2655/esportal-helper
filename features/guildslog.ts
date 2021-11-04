2c581af7a783cd4d1035e3297570df11dae66ce1
Jari <34107546+notJari@users.noreply.github.com>
2021-11-04 16:12:46 +0200

    server thumbnail for logging

--- features/guildslog.ts
+++ features/guildslog.ts
@@ -2,7 +2,7 @@ import { Channel, MessageEmbed } from 'discord.js';
 const logschannel = '905764848376369163'
 
 module.exports = (client: { on: (arg0: string, arg1: { (guild: any): void; (guild: any): void; }) => void; guilds: { cache: { size: any; }; }; channels: { cache: { get: (arg0: string) => { (): any; new(): any; send: { (arg0: { embeds: MessageEmbed[]; }): void; new(): any; }; }; }; }; }) => {
-    client.on('guildCreate', (guild: { name: any; id: any; memberCount: any; ownerId: any; }) => {
+    client.on('guildCreate', (guild: { name: any; id: any; memberCount: any; ownerId: any; iconURL: (arg0: { dynamic: boolean; }) => string; }) => {
     const join = new MessageEmbed()
         .setTitle('Added to Guild!')
         .addField('Guild Info:', `${guild.name} (${guild.id})`)
@@ -10,11 +10,12 @@ module.exports = (client: { on: (arg0: string, arg1: { (guild: any): void; (guil
         .addField('Owner Info:', `ID (${guild.ownerId})`)
         .setFooter(`Currently in ${client.guilds.cache.size} guilds!`)
         .setTimestamp()
+        .setThumbnail(guild.iconURL({ dynamic: true }))
         .setColor('GREEN')
     client.channels.cache.get(logschannel).send({embeds: [join]})
 })
-
-client.on('guildDelete', (guild: { name: any; id: any; memberCount: any; ownerId: any; }) => {
+// 
+client.on('guildDelete', (guild: { name: any; id: any; memberCount: any; ownerId: any; iconURL: (arg0: { dynamic: boolean; }) => string; }) => {
     const leave = new MessageEmbed()
         .setTitle('Removed from Guild!')
         .addField('Guild Info:', `${guild.name} (${guild.id})`)
@@ -22,6 +23,7 @@ client.on('guildDelete', (guild: { name: any; id: any; memberCount: any; ownerId
         .addField('Owner Info:', `ID (${guild.ownerId})`)
         .setFooter(`Currently in ${client.guilds.cache.size} guilds!`)
         .setTimestamp()
+        .setThumbnail(guild.iconURL({ dynamic: true }))
         .setColor('RED')
     client.channels.cache.get(logschannel).send({embeds: [leave]})
 })

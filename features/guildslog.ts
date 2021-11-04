2d347374238c45bd0dcca731aa56dbbac6c3f16c
Isidro Casiano <isidroICP@outlook.com>
2021-11-04 11:15:53 -0400

    feat: guild check

--- features/guildslog.ts
+++ features/guildslog.ts
@@ -3,21 +3,23 @@ const logschannel = '905764848376369163'
 
 module.exports = (client: Client) => {
     client.on('guildCreate', (guild: Guild) => {
-    if (!guild.available) return 
-    const join = new MessageEmbed()
-        .setTitle('Added to Guild!')
-        .addField('Guild Info:', `${guild.name} (${guild.id})`)
-        .addField('Guild Size:', `**${guild.memberCount}** members!`)
-        .addField('Owner Info:', `ID (${guild.ownerId})`)
-        .setFooter(`Currently in ${client.guilds.cache.size} guilds!`)
-        .setTimestamp()
-        .setThumbnail(guild.iconURL({ dynamic: true }))
-        .setColor('GREEN')
-    (client.channels.cache.get(logschannel) as TextChannel)?.send({embeds: [join]})
+        if (!guild) return;
+        if (!guild.available) return ;
+        const join = new MessageEmbed()
+            .setTitle('Added to Guild!')
+            .addField('Guild Info:', `${guild.name} (${guild.id})`)
+            .addField('Guild Size:', `**${guild.memberCount}** members!`)
+            .addField('Owner Info:', `ID (${guild.ownerId})`)
+            .setFooter(`Currently in ${client.guilds.cache.size} guilds!`)
+            .setTimestamp()
+            .setThumbnail(guild.iconURL({ dynamic: true }))
+            .setColor('GREEN');
+        (client.channels.cache.get(logschannel) as TextChannel)?.send({embeds: [join]});
 })
  
 client.on('guildDelete', (guild: Guild) => {
-    if (!guild.available) return 
+    if (!guild) return;
+    if (!guild.available) return;
     const leave = new MessageEmbed()
         .setTitle('Removed from Guild!')
         .addField('Guild Info:', `${guild.name} (${guild.id})`)
@@ -26,7 +28,7 @@ client.on('guildDelete', (guild: Guild) => {
         .setFooter(`Currently in ${client.guilds.cache.size} guilds!`)
         .setTimestamp()
         .setThumbnail(guild.iconURL({ dynamic: true }) ?? '')
-        .setColor('RED')
-    (client.channels.cache.get(logschannel) as TextChannel)?.send({embeds: [leave]})
+        .setColor('RED');
+    (client.channels.cache.get(logschannel) as TextChannel)?.send({embeds: [leave]});
 })
 }
